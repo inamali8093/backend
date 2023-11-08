20085d3642c74e631b3db5135dafbac64c02ad71
Inam <inamali0291@gmail.com>
2023-11-08 22:51:25 +0530

    connection to mongoDB atlas

---
+++ src/index.js
@@ -0,0 +1,23 @@
+//require('dotenv').config({path:'./env'})
+import dotenv from 'dotenv'
+import connectDB from './db/index.js';
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+
+
+
+/*
+;( async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    } catch (error) {
+        console.error("ERROR:",error);
+        throw error
+    }
+})()
+*/
