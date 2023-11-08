fd6a3d04ea982997902ccb23d71462fd26fad7bc
Inam <inamali0291@gmail.com>
2023-11-09 01:56:03 +0530

    utils construction

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 //require('dotenv').config({path:'./env'})
 import dotenv from 'dotenv'
 import connectDB from './db/index.js';
+import { app } from './app.js'
 
 dotenv.config({
     path: './env'
@@ -8,6 +9,19 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.on("error",(error)=>{  // here we are listening for the error event and calling a callback as per the event
+        console.log("ERRR:",error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is runnig at port : ${process.env.PORT}`);
+    })
+
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!!",err);
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,29 @@
+// const asyncHandler = ()=>{}
+// const asyncHandler = (fn)=>{}
+// const asyncHandler = (fn)=>{async ()=>{}}
+// const asyncHandler = (fn)=> async ()=>{}
+
+
+// it is a wrapper function used to interact with database everytime with async/await method
+/*
+const asyncHandler = (fn) => async (req,res,next)=>{
+    try {
+        await fn(req,res,next)
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+*/
+
+
+//promise method
+const asyncHandler = (requestHandler)=>{
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=> next(err))
+    }
+}
+
+export {asyncHandler}
